64207068841da5787261f1c02b60e999d4fe73d6
joboot <jordan.booth41900@gmail.com>
2021-09-22 16:12:45 -0500

    Finished Project steps 1-4. Coded all inputs along with data validation. Working while loop surrounding the whole program controlled by user input.

--- water_allocations_calc.py
+++ water_allocations_calc.py
@@ -5,4 +5,37 @@
 # Programmers: Jordan Booth, Mackenzie Eskey
 # Date: 2021.09.22
 
+LINE_LENGTH = 60
 
+print('Irrigation Water Allocations Calculator')
+print('=' * LINE_LENGTH)
+
+user_input = 'y'
+
+while user_input == 'y':
+
+    while True:
+        rained_allocation_depth = float(input('Enter rationed allocation depth(D) in inches: '))
+
+        if 0 < rained_allocation_depth:
+            break
+        else:
+            print('Invalid value. Please enter again.')
+
+    while True:
+        irrigated_area = float(input('Enter area being irrigated(A) in acres: '))
+
+        if 0 < irrigated_area:
+            break
+        else:
+            print('Invalid value. Please enter again.')
+
+    while True:
+        rained_allocation_depth = int(input('Enter average rate of flow(Q) in U.S. gpm: '))
+
+        if 0 < rained_allocation_depth:
+            break
+        else:
+            print('Invalid value. Please enter again.')
+
+    user_input = input('Would you like to make another calculation?(y/n): ')
