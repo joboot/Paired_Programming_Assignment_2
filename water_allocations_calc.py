a8f399e77c46402825ba6151d8107aac5bc46f0f
joboot <jordan.booth41900@gmail.com>
2021-09-22 15:52:25 -0500

    Initial heading and start of code

---
+++ water_allocations_calc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# A irrigation water allocations calculator for paired programming assignment 2
+# This program calculates and determines how long it will take to use up an allocation of water for irrigation.
+# Programmers: Jordan Booth, Mackenzie Eskey
+# Date: 2021.09.22
+
+
